0ee79f6f302f636333b506fc22e32da437419cd4
Hunter Miller <hmiller@mykey.com>
2020-05-17 13:42:12 -0600

    the snake has mulitple moving segments,s, eating food causes the snake to grow.

--- src/Game.js
+++ src/Game.js
@@ -6,53 +6,61 @@ const MOVE_RIGHT = 'ArrowRight';
 const MOVE_UP = 'ArrowUp';
 const MOVE_DOWN = 'ArrowDown';
 const DIRECTIONS = [MOVE_LEFT, MOVE_RIGHT, MOVE_UP, MOVE_DOWN];
-const COLS = 25;
-const ROWS = 25;
-export default function Game(props) {
-  const [food, setFood] = useState([getRandom(ROWS), getRandom(COLS)]);
-  const [score, setScore] = useState(0);
-  const direction = useDirection();
-  const snake = useSnake(direction);
+const WIDTH = 25;
+const HEIGHT = 25;
 
-  useEffect(() => {
-    if(snake[0] === food[0] && snake[1] === food[1]) {
-      setScore((prevScore) => prevScore + 1);
-      setFood([getRandom(ROWS), getRandom(COLS)])
-    }
-  }, [snake])
-  
-  function Tile({ isSnake, isFood }) {
+export default function Game() {
+
+  const direction = useDirection();
+  const { snake, food } = useSnake(direction);
+
+  function Cell({ isSnake, isFood }) {
     return(
       <div className={`cell ${isSnake ? 'snake' : ''} ${isFood ? 'food' : ''}`}></div>
     )
   }
 
-  function Row(props) {
-    const { row } = props;
+  function Row({ row }) {
+    let cells = [];
+    for(let i = 0; i < WIDTH; i++) {
+      cells.push(<Cell key={i}
+                       isSnake={snake.find((part) => part[0] === i && part[1] === row)}
+                       isFood={food[0] === i && food[1] === row} />)
+    }
 
     return(
       <div className="row">
-        {[...Array(COLS)].map((_, currCol) => { 
-          return <Tile key={currCol}
-                       isSnake={snake[0] === currCol && snake[1] === row}
-                       isFood={food[0] === currCol && food[1] === row} />
-        })}
+        {cells}
       </div>
     )
   }
 
+  let rows = []
+  for(let i = HEIGHT; i > 0; i--) {
+    rows.push(<Row row={i} key={i} />)
+  }
+
   return (
     <div>
-      <h3>Current Score {score}</h3>
       <div className="game">
-        {[...Array(ROWS)].map((_, i) => {
-          return (
-            <Row row={i} key={i} />
-          )
-        })}
+        {rows}
       </div>
     </div>
   );
+}
+
+function moveSegment(segment, direction) {
+  if(direction === MOVE_LEFT) {
+    return [segment[0] - 1 < 0 ? WIDTH : segment[0] - 1, segment[1]];
+  } else if(direction === MOVE_RIGHT) {
+    return [segment[0] + 1 > WIDTH ? 0 : segment[0] + 1, segment[1]];
+  } else if(direction === MOVE_UP) {
+    return [segment[0], segment[1] + 1 > HEIGHT ? 0 : segment[1] + 1];
+  } else if(direction === MOVE_DOWN) {
+    return [segment[0], segment[1] - 1 < 0 ? HEIGHT : segment[1] - 1];
+  } else {
+    return [...segment];
+  }
 }
 
 function useDirection() {
@@ -61,7 +69,16 @@ function useDirection() {
   useEffect(() => {
     const listener = document.addEventListener('keydown', (e) => {
       if(DIRECTIONS.includes(e.code)) {
-        setDirection(e.code);
+        setDirection((prevDirection) => {
+          if((prevDirection === MOVE_UP && e.code === MOVE_DOWN)    ||
+             (prevDirection === MOVE_DOWN && e.code === MOVE_UP)    ||
+             (prevDirection === MOVE_LEFT && e.code === MOVE_RIGHT) ||
+             (prevDirection === MOVE_RIGHT && e.code === MOVE_LEFT)) {
+            return prevDirection;
+          } else {
+            return e.code;
+          }
+        });
       }
     });
 
@@ -72,29 +89,33 @@ function useDirection() {
 }
 
 function useSnake(direction) {
-  const [snake, setSnake] = useState([getRandom(ROWS), getRandom(COLS)]);
+  const [snake, setSnake] = useState([[getRandom(HEIGHT), getRandom(WIDTH)]]);
+  const [food, setFood] = useState([getRandom(HEIGHT), getRandom(WIDTH)]);
 
+  // Add a segment when the snake eats a food
+  useEffect(() => {
+    if(snake.find((part) => part[0] === food[0] && part[1] === food[1])) {
+      setFood([getRandom(HEIGHT), getRandom(WIDTH)])
+      setSnake((prevSnake) => {
+        return [...prevSnake, moveSegment(prevSnake[prevSnake.length - 1], direction)];
+      });
+    }
+  }, [snake, food, direction])
+
+  // Move the snake segment by segment
   useEffect(() => {
     const tick = window.setInterval(() =>  {
-      switch(direction) {
-        case MOVE_LEFT:
-          setSnake((prevSnake) => [prevSnake[0] - 1 < 0 ? COLS : prevSnake[0] - 1, prevSnake[1]]);
-          break;
-        case MOVE_RIGHT:
-          setSnake((prevSnake) => [prevSnake[0] - 1 > COLS ? 0 : prevSnake[0] + 1, prevSnake[1]]);
-          break;
-        case MOVE_UP:
-          setSnake((prevSnake) => [prevSnake[0], prevSnake[1] - 1 < 0 ? COLS : prevSnake[1] - 1]);
-          break;
-        case MOVE_DOWN:
-          setSnake((prevSnake) => [prevSnake[0], prevSnake[1] - 1 > COLS ? 0 : prevSnake[1] + 1]);
-          break;
-      }
-    }, 25);
+      setSnake((prevSnake) => {
+        return prevSnake.map((segment, i) => {
+          return i === 0 ?  moveSegment(segment, direction) : [...prevSnake[i - 1]];
+        });
+      })
+
+    }, 50);
     return () => clearInterval(tick);
   })
 
-  return snake;
+  return { snake: snake, food: food };
 }
 
 function getRandom(max) {
